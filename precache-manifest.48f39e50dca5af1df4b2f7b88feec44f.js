a15a67ebd0fe66f66640d01ba49abbbf88d86986
Muneeb T <muneebmnb17@gmail.com>
2022-07-30 13:42:03 +0530

    Updates

---
+++ precache-manifest.48f39e50dca5af1df4b2f7b88feec44f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a5434f49ecfb0732284",
+    "url": "/Muneeb-T/React-netflix-simple-clone/static/css/main.a4a38f01.chunk.css"
+  },
+  {
+    "revision": "5a5434f49ecfb0732284",
+    "url": "/Muneeb-T/React-netflix-simple-clone/static/js/main.5a5434f4.chunk.js"
+  },
+  {
+    "revision": "53bb4e2bc0d7f895da2b",
+    "url": "/Muneeb-T/React-netflix-simple-clone/static/js/1.53bb4e2b.chunk.js"
+  },
+  {
+    "revision": "7d1e83e2adae25597907",
+    "url": "/Muneeb-T/React-netflix-simple-clone/static/js/runtime~main.7d1e83e2.js"
+  },
+  {
+    "revision": "f631bedeb825e86ba6b08b2c8c77ff46",
+    "url": "/Muneeb-T/React-netflix-simple-clone/index.html"
+  }
+];
